cdc130ff1431302316424bafeca8902beb84fa19
Finlay <finlay.mitchner@gmail.com>
2025-04-14 11:07:49 -0700

    complete

--- task4.py
+++ task4.py
@@ -1,14 +1,15 @@
 import tkinter as tk
 from tkinter import *
 window = tk.Tk()
-window.title("Example")
-#entities
+window.title("example")
+window.geometry("260x150")
+#Entities
 name = tk.Label(window, text = "Pochacco!")
 photo = PhotoImage(file = "dog.png")
 dog = tk.Label(window, image = photo)
 text = tk.Label(window, text = "A cuddly little puppy! This is from the same \n creators who brought you Keropi and Kero Kero",bg = "#87CEEB" )
-#position
-name.grid(row = 3, column = 4, sticky=W)
-dog.grid(row = 3, column=3)
-text.grid(row = 4, column = 1, columnspan = 5, rowspan = 2)
+#positions
+dog.place(x = 120, y = 70, anchor=E)
+name.place(x = 125, y= 70, anchor = W)
+text.place(x = 130, y = 150, anchor = S)
 window.mainloop()
